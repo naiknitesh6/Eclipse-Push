623c93832531bea95c0bf71d5ab50d4ee141c9d7
naiknitesh6@gmail.com <naiknitesh6@gmail.com>
2023-04-21 15:10:03 +0530

    second commit

---
+++ FlywayPractise/src/test/java/com/example/FlywayPractise/FlywayPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlywayPractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
